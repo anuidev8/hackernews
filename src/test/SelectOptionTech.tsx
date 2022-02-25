b612f196d0c5d630284aedf90f057027976af07c
aarrieta3 <aarrieta@undergrub.com>
2022-02-25 18:34:35 -0500

    :sparkles: update selectOptions test in onchage method

--- src/test/SelectOptionTech.tsx
+++ src/test/SelectOptionTech.tsx
@@ -1,14 +1,16 @@
 import react from 'react'
-import { render,cleanup } from '@testing-library/react'
+import { render,cleanup,fireEvent, waitFor} from '@testing-library/react'
 
 import  SelectOptionTech from '../components/SelectOptionTech'
 
 describe('PostCard component', ()=>{
 
- /*    const selectOptions = [
-        {icon:'react',t}
+    const selectOptions = [
+        {icon:'react',title:'react'},
+        {icon:'vue',title:'vue'},
+        {icon:'vue',title:'angular'},
     ]
- */
+ 
     let mainWrapper : any
 
     beforeEach(()=>{
@@ -35,7 +37,23 @@ describe('PostCard component', ()=>{
     })
     
     //ACTIONS
-  
+    it('should call onChange when the option is selected', async()=>{
+            const mockedOnchange = jest.fn()
+            render(<SelectOptionTech onChange={ mockedOnchange} optionsItems={selectOptions} />)
+            
+            const SelectButton = mainWrapper.container.querySelector('button')
+            fireEvent.click(SelectButton)
+
+            const techsOptions = mainWrapper.container.querySelector('.selectOption__techs--show')
+            await waitFor(()=>techsOptions)   
+            fireEvent.click(techsOptions)
+
+            expect(mockedOnchange).toHaveBeenCalledTimes(1)
+
+            
+
+
+    })
 
     afterAll(cleanup)
 })
