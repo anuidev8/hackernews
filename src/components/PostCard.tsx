e290970fa88a8cdfb9c5e750221732894c9a5f00
aarrieta3 <aarrieta@undergrub.com>
2022-02-26 03:45:55 -0500

    :sparkles: update postcard design

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,5 +1,9 @@
 import { FC } from 'react'
 
+import Styles from '../styles/Postcard.module.css'
+//Components
+import { Icons } from './Icons'
+
 interface Props {
     author?:string
     story_title?:string
@@ -8,11 +12,30 @@ interface Props {
 
 }
 
-const PostCard : FC<Props> = ({story_title})=>{
+const PostCard : FC<Props> = ({author,story_title,story_url,created_at})=>{
+    //NECESITAMOS CAMBIAR EL TEST VALIDAR LA URL EL LA URL
     return(
-       <section>
-           <h2>{story_title}</h2>
-       </section>
+       <article data-testid={story_url} className={`${Styles.postCard} d-flex`}>
+           <div className={`${Styles.postCard__wrapper}`}>
+           <div className={`${Styles.postCard__header}  d-flex align-center`} >
+               <figure className='d-flex align-center'>
+                    <Icons.TimeIcon  />
+               </figure>
+               <span>{created_at} by {author}</span>
+           </div>
+           <p className={`${Styles.postCard__content}`}>
+           {story_title}
+           </p>
+           </div>
+           <ul className={`${Styles.postCard__actions}`}>
+                <li className='h-full'>
+                    <button className='h-full d-flex justify-center align-center'>
+                        
+                        <Icons.HeartIcon  />
+                    </button>
+                </li>
+           </ul>
+       </article>
     )
 }
 
